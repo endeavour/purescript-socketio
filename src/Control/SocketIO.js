95cbf1b52d595185603ef2e86dda735a5a8ef3f1
James Freiwirth <james@perfectshuffle.co.uk>
2015-12-19 02:42:18 +0000

    Fix socketio.js

--- src/Control/SocketIO.js
+++ src/Control/SocketIO.js
@@ -1,28 +1,28 @@
 // module Control.SocketIO
 
-export.stringify = function stringify(x) {
+exports.stringify = function stringify(x) {
     return JSON.stringify(x);\
   };
 
-export.connect = function connect(host) {
+exports.connect = function connect(host) {
     return function() {
       return io.connect(host);
     };
   };
 
-export.onImpl = function onImpl(sock, channel, onMessage) {
+exports.onImpl = function onImpl(sock, channel, onMessage) {
     return function() {
       sock.on(channel, function(m){ onMessage(m)();});
     };
   };
 
-export.emitImpl = function emitImpl(sock, channel) {
+exports.emitImpl = function emitImpl(sock, channel) {
     return function() {
       sock.emit(channel);
     };
   };
 
-export.emitMsgImpl = function emitMsgImpl(sock, channel, emitMessage) {
+exports.emitMsgImpl = function emitMsgImpl(sock, channel, emitMessage) {
     return function() {
       sock.emit(channel, emitMessage);
     };
